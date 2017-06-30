8cd2afa85b2df69013d51911868629e4857dd26c
jovanrudic <m1rudic@gmail.com>
2017-06-30 21:29:04 +0200

    jr

--- app/src/main/java/com/projectx/jovanrudic/mhydrabanking/PhoneUtility.java
+++ app/src/main/java/com/projectx/jovanrudic/mhydrabanking/PhoneUtility.java
@@ -1,8 +1,23 @@
 package com.projectx.jovanrudic.mhydrabanking;
 
+import android.content.Context;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+
 /**
  * Created by jovanrudic on 6/30/17.
  */
 
 public class PhoneUtility {
+
+    public static PhoneModel getPhoneData(Context context){
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context
+                .TELEPHONY_SERVICE);
+
+        String imei =  telephonyManager.getDeviceId();
+        String phoneNumber = telephonyManager.getLine1Number();
+        String os = Build.VERSION.RELEASE;
+        String deviceModel = Build.MODEL;
+        return new PhoneModel(imei, phoneNumber, deviceModel, os);
+    }
 }
