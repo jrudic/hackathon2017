aa35a5f39e0588e14341af53b6ae5ba83e47ef8d
urossimic <simic.uros@gmail.com>
2017-06-30 21:55:02 +0200

    Added endpoint

---
+++ app/src/main/java/com/projectx/jovanrudic/mhydrabanking/api/Endpoint.java
@@ -0,0 +1,13 @@
+package com.projectx.jovanrudic.mhydrabanking.api;
+
+/**
+ * Created by urossimic on 6/30/17.
+ */
+
+public class Endpoint {
+
+    public static final String BASE_URL = "www.pornhub.com"; //todo
+
+    // example
+    public static final String USER = BASE_URL + "/user";
+}
